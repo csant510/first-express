f85745a8604d17416b0a10bd40a94e915ea21a3b
csant510 <james.sant83@gmail.com>
2020-11-14 17:06:45 -0800

    wrote the obligatory Hello World application

---
+++ server.js
@@ -0,0 +1,16 @@
+//load express module
+const express = require('express');
+
+//create out express app
+const app = express();
+
+ // Define a "root" route directly on app
+ app.get('/', function (req, res) {
+    res.send('<h1>Hello World!</h1>');
+  });
+
+// Tell the app to listen on port 3000
+ // for HTTP requests from clients
+ app.listen(3000, function () {
+    console.log('Listening on port 3000');
+  });
