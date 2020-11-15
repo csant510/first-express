e7b3130cc5220f10ed0c2408efd4d32de7fb9225
csant510 <james.sant83@gmail.com>
2020-11-14 19:35:41 -0800

    added a another route for pratice

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const app = express();
 
  // Define a "root" route directly on app
  app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>');
+    res.send('<h1>Hello Express!</h1>');
   });
+
+  app.get('/home',function(req, res){
+      res.send('<h1>Home Page</h1>')
+  })
+
 
 // Tell the app to listen on port 3000
  // for HTTP requests from clients
